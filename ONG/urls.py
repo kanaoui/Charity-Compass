e66982571800338b3aa25532767d067baf77a7c3
Mohamed-Badr77 <mohamedbadrrhezouane@gmail.com>
2024-04-19 11:07:20 +0100

    change url

--- ONG/urls.py
+++ ONG/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/hi', admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
     path('campagne/', include('campagne.urls')),
 ]
