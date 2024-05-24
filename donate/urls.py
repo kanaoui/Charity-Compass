30efb9ec3c48963e3355a447ff370ff28bc2387e
Mohamed-Badr77 <mohamedbadrrhezouane@gmail.com>
2024-05-24 04:26:38 +0100

    unfinished Website

--- donate/urls.py
+++ donate/urls.py
@@ -6,4 +6,6 @@ app_name='donate'
 
 urlpatterns=[
     re_path(r'^donate/$',views.donate_view, name="donate"),
+    re_path(r'^(?P<slug>[\w-]+)/$', views.donate_detail, name="ddonate"),
+    
 ]
--- donate/views.py
+++ donate/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from campagne.models import Campagne
 
 # Create your views here.
 
 
 def donate_view(request):
     return render(request,'donate/donate.html')
+
+def donate_detail(request, slug):
+    # return HttpResponse(slug)
+    campagne = Campagne.objects.get(slug=slug)
+    return render(request, 'donate/donate.html', { 'campagne': campagne })
